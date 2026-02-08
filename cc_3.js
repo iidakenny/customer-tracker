9f653b5ad1e6404a48b6d41f4448aba53d714fdb
Iida Kenny <iida@usf.edu>
2026-02-08 12:06:48 -0500

    Created the initial files and the customer array.

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+let customers = [
+    {name: "Matti Pallopää", email: "matti.pallopaa@icloud.com", purchases: ["water", "bread", "coffee"]},
+    {name: "Päivi Hollola", email: "paivi.hollola@icloud.com", purchases: ["beef", "cucumber", "paper towels"]},
+    {name: "Eetu Kimurainen", email: "eetu.kimurainen@icloud.com", purchases: ["croissant", "cheese", "jam"]}
+];
+            
